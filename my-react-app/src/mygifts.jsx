8701a9576922f842a10a48f604c593b189d9b6c7
www-getgems <ivm4ik@gmail.com>
2025-12-21 02:09:41 +0200

    Update mygifts.jsx

--- my-react-app/src/mygifts.jsx
+++ my-react-app/src/mygifts.jsx
@@ -1,22 +1,26 @@
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Header from './header';
 import Menu from './menu';
 import Card from './card';
 
 export default function MyGifts() {
-  
+  const navigate = useNavigate();
 
   return (
-    <div className='h-screen text-white font-sans overflow-auto bg-[#121921] '>
-        <Header />
-        <div className='ml-3 mt-3 text-2xl font-semibold'>
-            <h1 className='ml-3'>Мои подарки</h1>
-            <div className='grid grid-cols-2'>
-                <Card text="Забрать" collection="lootbag" nft_id={539} />
-            </div>
+    <div className='h-screen text-white font-sans overflow-auto bg-[#121921]'>
+      <Header />
+      <div className='ml-3 mt-3 text-2xl font-semibold'>
+        <h1 className='ml-3'>Мои подарки</h1>
+        <div className='grid grid-cols-2'>
+          <Card 
+            text="Забрать" 
+            collection="lootbag" 
+            nft_id={539} 
+            onClick={() => navigate("/auth")} 
+          />
         </div>
-        <Menu />
+      </div>
+      <Menu />
     </div>
   );
 }
-
